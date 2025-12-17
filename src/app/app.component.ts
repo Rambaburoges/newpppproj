57c37f25dbe483c99c55834d2a807975919fea40
Rambabu Ramaraj <ramprasath913@gmail.com>
2025-12-17 22:45:43 +0530

    add soem keywords

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';  // Import here [web:74][web:75]
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,31 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ppp';
+export class AppComponent implements OnInit,AfterViewInit
+ {
+  constructor(private meta: Meta, private title: Title) {}  // Dependency injection [web:77]
+  ngOnInit(): void {
+    this.title.setTitle('Steel Scoring Dies India | Precision Makers');
+  this.meta.updateTag({ 
+      name: 'description', 
+      content: 'Custom scoring dies for precision packaging & sheet metal in India. High-quality steel rotary tools from Sivakasi makers. Fast turnaround, ISO certified. Get your free quote today!' 
+    });  
+  }
+
+
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // const schema = {
+    //   "@type": "Product",
+    //   "name": "Rotary Scoring Dies Sivakasi",
+    //   "description": "Precision rotary scoring dies for cardboard packaging India"
+    // };
+    // const script = document.createElement('script');
+    // script.type = 'application/ld+json';
+    // script.text = JSON.stringify(schema);
+    // document.head.appendChild(script);//Add 'implements AfterViewInit' to the class.
+    
+  }
+
 }
